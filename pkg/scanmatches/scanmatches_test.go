d3e0d77d5c2ed7d52beed570a325cf69e517f3c2
davebehr1 <davebehr1@gmail.com>
2022-02-02 15:46:48 +0200

    added matcher to test to test if the ranks are calculated correctly

--- pkg/scanmatches/scanmatches_test.go
+++ pkg/scanmatches/scanmatches_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/davebehr1/spanassessment/pkg/scanmatches"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
 )
 
 func TestScanMatches(t *testing.T) {
@@ -15,6 +16,30 @@ func TestScanMatches(t *testing.T) {
 		matches, err := rankTable.ScanFromFile("../../matches.txt")
 		g.Expect(err).To(BeNil())
 		g.Expect(len(matches)).To(Equal(7))
+
+		g.Expect(matches).To(ContainElements(
+			PointTo(MatchFields(IgnoreExtras, Fields{
+				"Rank": Equal(11), "Team": Equal("Lions"),
+			})),
+			// PointTo(MatchAllFields(Fields{
+			// 	"Team": Equal("Tarantulas"), "Rank": Equal(6),
+			// })),
+			// PointTo(MatchAllFields(Fields{
+			// 	"Team": Equal("FC Awesome"), "Rank": Equal(1),
+			// })),
+			// PointTo(MatchAllFields(Fields{
+			// 	"Team": Equal("Snakes"), "Rank": Equal(1),
+			// })),
+			// PointTo(MatchAllFields(Fields{
+			// 	"Team": Equal("Grouches"), "Rank": Equal(0),
+			// })),
+			// PointTo(MatchAllFields(Fields{
+			// 	"Team": Equal("Pumas"), "Rank": Equal(0),
+			// })),
+			// PointTo(MatchAllFields(Fields{
+			// 	"Team": Equal("Cheetas"), "Rank": Equal(0),
+			// })),
+		))
 
 	})
 	t.Run("Scan matches from stdin", func(t *testing.T) {
